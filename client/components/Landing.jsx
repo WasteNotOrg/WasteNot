c7b2e72583ff6696ba9b4695af1108568dca7de9
Kit Loong Yee <kitloong@gmail.com>
2021-02-11 02:05:56 -0600

    add logo and login form to center

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import '../public/css/styles.css';
+import logo from '../public/images/logo.png';
 
 const Landing = () => {
   // LOCAL STATE
@@ -28,44 +30,47 @@ const Landing = () => {
   // const invokeSignup= () => { // TODO
   //   return
   // }  
- return (
-  <Form>
-    <Form.Group controlId="formBasicEmail">
-      <Form.Label>Email address</Form.Label>
-      <Form.Control 
-        type="email" 
-        placeholder="Enter email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <Form.Text className="text-muted">
-        We'll never share your email with anyone else.
-      </Form.Text>
-    </Form.Group>
+  return (
+    <div id="landing">
+      <img id="logo" alt="logo" src={logo}></img>
+      <Form id ="form">
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control 
+            type="email" 
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
 
-    <Form.Group controlId="formBasicPassword">
-      <Form.Label>Password</Form.Label>
-       <Form.Control
-         type="password"
-         placeholder="Password"
-         value={password}
-         onChange={(e) => setPassword(e.target.value)} />
-    </Form.Group>
-    <Button 
-      variant="primary" 
-      type="button"
-      onClick={(e) => invokeLogin()} // ! this might be wrong
-    >
-      Login
-    </Button>
-    <Button 
-      variant="primary" 
-      type="button"
-      onClick={(e) => invokeSignup()} // ! this is not fully implemented
-    >
-      Signup
-    </Button>
-  </Form>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)} />
+        </Form.Group>
+        <Button 
+          variant="primary" 
+          type="button"
+          onClick={(e) => invokeLogin()} // ! this might be wrong
+        >
+          Login
+        </Button>
+        <Button 
+          variant="primary" 
+          type="button"
+          onClick={(e) => invokeSignup()} // ! this is not fully implemented
+        >
+          Signup
+        </Button>
+      </Form>
+    </div>
   );
 };
 export default Landing;
