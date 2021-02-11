b5ff3e304f80f36c526e0f2fe3efac6b5d6d0e3b
Courtney <777.catalyst@gmail.com>
2021-02-11 13:13:23 -0600

    Filter based on donator status

--- server/controllers/feedController.js
+++ server/controllers/feedController.js
@@ -2,30 +2,28 @@ const db = require('../models/model.js');
 
 const feedController = {};
 
-feedController.getUsers = (req, res, next) => {
-  const getQuery = 'SELECT * FROM user_info';
-  db.query(getQuery)
-    .then((users) => {
-      res.locals.users = users.rows;
-      return next();
-    })
-    .catch((err) => next({ error: err }));
-};
+// feedController.getUsers = (req, res, next) => {
+//   const getQuery = 'SELECT * FROM user_info';
+//   db.query(getQuery)
+//     .then((users) => {
+//       res.locals.users = users.rows;
+//       return next();
+//     })
+//     .catch((err) => next({ error: err }));
+// };
 
+// get users based on donating status
 feedController.getFilteredUsers = (req, res, next) => {
-  // get email from verified user
+  // get the email from verified user
   const { email } = res.locals.email;
   const query = `SELECT * FROM user_info WHERE email = ${email}`;
-  // query db based on user's email to get all user info
+  // query the db based on user email to get all user info
   db.query(query)
     .then((response) => response.json())
-    .then((data) => {
-      // res.locals.user = data.row;
-      // return next();
-
+    .then((user) => {
       // check if the donator status of the user is true
-      if (data.donator === true) {
-        // if so, then query db for all users who have a donator status of false
+      if (user.donator === true) {
+        // if true, then query db for all users who have a donator status of false
         const donatorQuery = 'SELECT * FROM user_info WHERE donator = false';
         db.query(donatorQuery)
           .then((users) => {
@@ -34,7 +32,7 @@ feedController.getFilteredUsers = (req, res, next) => {
           })
           .catch((err) => next({ error: err }));
       }
-      // else, query db for all users with a donator status of true.
+      // if status is false, query db for every user with a donator status of true.
       else {
         const donatorQuery = 'SELECT * FROM user_info WHERE donator = true';
         db.query(donatorQuery)
